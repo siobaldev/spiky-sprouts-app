5f017ecd06d9cbc352bef5ad6cdda13c6447a860
br0wnrecluse <sblmnrd@gmail.com>
2024-09-29 23:12:07 +0800

    setup new arrival section

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "@/components/hero/Page";
 import Testimonial from "@/components/testimonial/Page";
+import NewArrival from "@/components/newArrival/Page";
 
 export default function Home() {
   return (
     <main className="mx-auto">
       <Hero />
       <Testimonial />
+      <NewArrival />
     </main>
   );
 }
