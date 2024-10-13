d0f5127f5538848de105b219b2d8f59d4854e3b2
br0wnrecluse <sblmnrd@gmail.com>
2024-10-13 16:58:47 +0800

    Contact page initial setup

---
+++ app/contact/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section className="mb-12 flex items-center justify-center gap-y-4 pt-16 md:pt-28 lg:pt-40">
+      Contact Page
+    </section>
+  );
+}
