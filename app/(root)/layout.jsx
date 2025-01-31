ded3dc935713937308ddd55ac48f9a7aab1e7699
br0wnrecluse <sblmnrd@gmail.com>
2025-02-01 04:48:57 +0800

    added FormDataProvider to layout

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import Header from "@/components/header/Page";
 import Footer from "@/components/footer/page";
 import { CartProvider } from "@/context/CartProvider";
+import { FormDataProvider } from "@/context/FormDataProvider";
 import { Toaster } from "sonner";
 
 export default function Layout({ children }) {
   return (
     <CartProvider>
-      <Header />
-      {children}
-      <Footer />
-      <Toaster />
+      <FormDataProvider>
+        <Header />
+        {children}
+        <Footer />
+        <Toaster />
+      </FormDataProvider>
     </CartProvider>
   );
 }
